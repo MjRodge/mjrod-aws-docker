8b1f19f1390755629e44f2690aea3c6e053ddf0f
mjrod <michael@1mrkt.to>
2022-06-16 16:18:53 -0400

    specified a static, private ip

--- prod/services/public-ec2/docker-ec2.tf
+++ prod/services/public-ec2/docker-ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "docker-host" {
   ami = "ami-02d1e544b84bf7502" # Amazon Linux 2 AMI (HVM) - Kernel 5.10
   instance_type = "t2.micro"
   subnet_id = data.terraform_remote_state.public_subnet.outputs.public_subnet_id
+  private_ip = "10.0.1.1"
 
   tags = {
     Name = "mjrod-aws-docker-host"
