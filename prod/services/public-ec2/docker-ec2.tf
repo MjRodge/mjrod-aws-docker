9659a23245ea3b40e43fdaa56df6d885d374d8da
mjrod <michael@1mrkt.to>
2022-06-16 16:50:55 -0400

    added public ip association for testing

--- prod/services/public-ec2/docker-ec2.tf
+++ prod/services/public-ec2/docker-ec2.tf
@@ -1,11 +1,16 @@
-resource "aws_instance" "docker-host" {
+resource "aws_instance" "docker_host" {
   # us-east-2
   ami = "ami-02d1e544b84bf7502" # Amazon Linux 2 AMI (HVM) - Kernel 5.10
   instance_type = "t2.micro"
   subnet_id = data.terraform_remote_state.public_subnet.outputs.public_subnet_id
   private_ip = "10.0.1.4"
+  associate_public_ip_address = true
 
   tags = {
     Name = "mjrod-aws-docker-host"
   }
+}
+
+output "docker_host_public_ip" {
+  value = aws_instance.docker_host.public_ip
 }
