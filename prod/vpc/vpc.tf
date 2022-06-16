676310f74a3a1e59f5b3380d76ee43a9cc75a619
mjrod <michael@1mrkt.to>
2022-06-16 09:58:58 -0400

    added internet gateway resource creation

--- prod/vpc/vpc.tf
+++ prod/vpc/vpc.tf
@@ -7,11 +7,18 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+# Public subnet resource
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
+  depends_on = [aws_internet_gateway.igw]
 
   tags = {
     Name = "mjrod-aws-docker-public-subnet"
   }
+}
+
+# Internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 }
