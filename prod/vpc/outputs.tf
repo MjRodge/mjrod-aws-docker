9b90eb14daab344d9a693d0f1b074c1b85dea6c4
mjrod <michael@1mrkt.to>
2022-06-16 21:07:13 -0400

    output elastic ip public address

--- prod/vpc/outputs.tf
+++ prod/vpc/outputs.tf
@@ -2,3 +2,7 @@ output "public_subnet_id" {
   value = aws_subnet.public.id
   description = "public subnet resource id"
 }
+
+output "eip_public_ip_address" {
+  value = aws_eip.public_entry_ip.public_ip
+}
