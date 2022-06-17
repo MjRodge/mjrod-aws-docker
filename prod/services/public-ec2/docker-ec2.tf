7249f6bb56accc4fea7355fb27b7d49538317732
mjrod <michael@1mrkt.to>
2022-06-16 20:26:37 -0400

    added output for temporary public ip

--- prod/services/public-ec2/docker-ec2.tf
+++ prod/services/public-ec2/docker-ec2.tf
@@ -3,14 +3,10 @@ resource "aws_instance" "docker_host" {
   ami = "ami-02d1e544b84bf7502" # Amazon Linux 2 AMI (HVM) - Kernel 5.10
   instance_type = "t2.micro"
   subnet_id = data.terraform_remote_state.public_subnet.outputs.public_subnet_id
-  private_ip = "10.0.1.4"
+  private_ip = "10.0.1.5"
   associate_public_ip_address = true
 
   tags = {
     Name = "mjrod-aws-docker-host"
   }
-}
-
-output "docker_host_public_ip" {
-  value = aws_instance.docker_host.public_ip
 }
--- prod/services/public-ec2/outputs.tf
+++ prod/services/public-ec2/outputs.tf
@@ -2,3 +2,7 @@ output "docker_host_id" {
   value = aws_instance.docker_host.id
   description = "id of the docker host instance"
 }
+
+output "docker_host_public_ip" {
+  value = aws_instance.docker_host.public_ip
+}
