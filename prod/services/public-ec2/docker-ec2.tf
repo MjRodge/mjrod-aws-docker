b0270e639bc031aac21e011a6a431b398f0579da
mjrod <michael@1mrkt.to>
2022-06-23 13:04:13 -0400

    updated remote state variable name

--- prod/services/public-ec2/docker-ec2.tf
+++ prod/services/public-ec2/docker-ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "docker_host" {
   # us-east-2
   ami = "ami-02d1e544b84bf7502" # Amazon Linux 2 AMI (HVM) - Kernel 5.10
   instance_type = "t2.micro"
-  subnet_id = data.terraform_remote_state.public_subnet.outputs.public_subnet_id
+  subnet_id = data.terraform_remote_state.prod_vpc.outputs.public_subnet_id
   private_ip = "10.0.1.5"
   associate_public_ip_address = true
 
