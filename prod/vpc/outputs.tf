675d3a53d052f9d3e2a4648cb12156179c6369e1
mjrod <michael@1mrkt.to>
2022-06-23 12:56:25 -0400

    output vpc public security group id

--- prod/vpc/outputs.tf
+++ prod/vpc/outputs.tf
@@ -5,4 +5,10 @@ output "public_subnet_id" {
 
 output "eip_public_ip_address" {
   value = aws_eip.public_entry_ip.public_ip
+  description = "elastic ip public address"
+}
+
+output "public_security_group" {
+  value = aws_security_group.public_subnet_security_group.id
+  description = "public security group id"
 }
