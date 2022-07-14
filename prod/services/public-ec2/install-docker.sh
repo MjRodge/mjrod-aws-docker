0a27676d0ce6def9a0ac61c164a4482a59a0cd51
mjrod <michael@1mrkt.to>
2022-07-14 12:17:36 -0400

    install docker bash script starting point

---
+++ prod/services/public-ec2/install-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# upgrade host
+sudo dnf upgrade -y
+
+# install yum-utils
+sudo dnf install -y yum-utils
+
+# add docker repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# remove conflicting packages
+sudo dnf remove -y buildah podman
+
+# install docker packages
+sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+# enable services in systemd
+sudo systemctl start docker && sudo systemctl enable docker
+sudo systemctl enable containerd
+
+# post docker install steps
+# manage docker as non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
